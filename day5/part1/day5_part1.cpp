497316e2f1f1f76a3d4d6c04e90f367d018f4539
DebalSahaCodes <116924730+DebalSahaCodes@users.noreply.github.com>
2024-12-19 23:45:41 -0800

    Update day5_part1.cpp
    Fixed the computation of middle page-number position

--- day5/part1/day5_part1.cpp
+++ day5/part1/day5_part1.cpp
@@ -14,22 +14,12 @@ int main()
     std::list<PGSVEC> pages_list;
     get_candidate_rules_and_pages(fileP, rule_book, pages_list);
 
-    /*std::string pg = "75,47,61,53,29";
-    //std::string pg = "75,97,47,61,53";
-
-    PGSVEC pg_vec = get_pages_vec(pg);
-    for(const auto s: pg_vec)
-        std::cout << s << " ";
-    std::cout << "\n";
-    std::cout << (check_pg_numbers_vs_rules(pg_vec, rule_book) ? "Good" : "Bad") << "\n";*/
-
     int middle_sum = 0;
     for(const PGSVEC& pages : pages_list)
     {
         if(check_pg_numbers_vs_rules(pages, rule_book))
         {
-            int rem = (pages.size()%2 == 0) ? 0 : 1;
-            int midpos = pages.size()/2 + rem;
+            int midpos = pages.size()/2;
             auto midnum = pages[midpos];
             std::cout << "Milldle : " << midnum << "\n";
             middle_sum += std::stoi(midnum);
