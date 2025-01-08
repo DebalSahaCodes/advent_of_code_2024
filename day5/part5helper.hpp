f2b357c8fa45e409ddb56600e1db1ee8d5a31873
DebalSahaCodes <116924730+DebalSahaCodes@users.noreply.github.com>
2025-01-07 23:24:08 -0800

    Update part5helper.hpp
    Updated helper file with a new comparison function to be used to sort the pages

--- day5/part5helper.hpp
+++ day5/part5helper.hpp
@@ -1,7 +1,8 @@
 #include<iostream>
+#include<fstream>
 #include<vector>
 #include<string>
-#include<fstream>
+
 
 int is_numeral(const char& c)
 {
@@ -35,17 +36,18 @@ int get_num(const std::string& str)
 
 using PAGENUM = std::string; // page-no.
 using PGSVEC = std::vector<PAGENUM>;
+using PGSVECLST = std::list<PGSVEC>;
 struct RULEDATA;
-using RULVEC = std::vector<RULEDATA>;
+using RULESBOOK = std::list<RULEDATA>;
 
 struct RULEDATA
 {
-    std::string _pg_before;
-    std::string _pg_after;
-    std::string _pg_number;
+    std::string _pg1st;
+    std::string _pg2nd;
 
-    static void make_rule(const std::string& inSTR, RULEDATA& rule1, RULEDATA& rule2)
+    static const RULEDATA make_rule(const std::string& inSTR)
     {
+        RULEDATA rule;
         const size_t pos = inSTR.find("|");
 
         if(pos != std::string::npos)
@@ -54,17 +56,15 @@ struct RULEDATA
             auto str2 = inSTR.substr(pos+1, inSTR.size() - pos - 1);
             if(get_num(str1) && get_num(str2))
             {
-                rule1._pg_number  = str1;
-                rule1._pg_after = str2;
-
-                rule2._pg_number   = str2;
-                rule2._pg_before = str1;
+                rule._pg1st = str1;
+                rule._pg2nd = str2;
             }
             else
             {
                 std::cout << "Bad rule candidate: \"" + inSTR + "\"\n";
             }
         }
+        return rule;
     }
 
     static bool is_according_to_rule(const std::pair<PAGENUM, PAGENUM>& pgPair, const RULEDATA& ruleD)
@@ -75,59 +75,116 @@ struct RULEDATA
         //std::cout << "\nPage 1: " << pg1st << "\n";
         //std::cout << "\nPage 2: " << pg2nd << "\n\n";
 
-        if(pg1st == pg2nd || pg1st != ruleD._pg_number && pg2nd != ruleD._pg_number)
-        {
-            return true;
-        }
-        else
-        {
-            bool isOK = pg1st == ruleD._pg_number && (pg2nd == ruleD._pg_after  || pg2nd != ruleD._pg_before)
-                    ||  pg2nd == ruleD._pg_number && (pg1st == ruleD._pg_before || pg2nd != ruleD._pg_after);
-            return isOK;
-        }
+        bool isNotOK = pg1st == ruleD._pg2nd && pg2nd == ruleD._pg1st;
+        return !isNotOK;
     }
 
     RULEDATA(){}
     RULEDATA(const RULEDATA& data)
     {
-        this->_pg_before = data._pg_before;
-        this->_pg_after = data._pg_after;
-        this->_pg_number = data._pg_number;
+        this->_pg1st = data._pg1st;
+        this->_pg2nd = data._pg2nd;
     }
     RULEDATA& operator=(const RULEDATA& data)
     {
-        this->_pg_before = data._pg_before;
-        this->_pg_after = data._pg_after;
-        this->_pg_number = data._pg_number;
+        this->_pg1st = data._pg1st;
+        this->_pg2nd = data._pg2nd;
+    }
+    void print_rule() const
+    {
+        std::cout << _pg1st << "|" << _pg2nd;
     }
 };
 
 
 
+bool compare_pg1_lt_pg2_using_rules(
+    const PAGENUM& pg1,
+    const PAGENUM& pg2,
+    const RULESBOOK& rule_book)
+{
+    bool isDone = false;
+    bool isLess = false;
+    auto ruleIter0 = rule_book.begin();
+    for(; ruleIter0!=rule_book.end() || !isDone; ++ruleIter0)
+    {
+        if(ruleIter0->_pg1st == pg1 && ruleIter0->_pg2nd == pg2)
+        {
+            isLess = true;
+            isDone = true;
+        }
+        else if(ruleIter0->_pg1st == pg2 && ruleIter0->_pg2nd == pg1)
+        {
+            isDone = true;
+        }
+    }
+    if(!isDone) std::cout << "Couldn't find";
+    return isLess;
+}
+
 bool check_pg_numbers_vs_rules(
     const PGSVEC& pages,
-    const RULVEC& rule_book)
+    const RULESBOOK& rule_book,
+    const int nRules,
+    bool doPrint)
 {
+    int till = (nRules>=0 && nRules <= rule_book.size()) ? nRules : static_cast<int>(rule_book.size());
+    if(doPrint) std::cout << "\nUsing \"till\" value : " << till << "\n";
+
     if(pages.empty())
     {
+        std::cout << "\nEMPTY PAGES VEC GIVEN TO CHECK!!!\n";
         return false;
     }
     else if(pages.size() == 1)
     {
+        std::cout << "\nSINGLE PAGE IN THE PAGES VEC!\n";
         return true;
     }
     bool isOK = true;
-    for(int i=0; i<pages.size()-1; ++i)
+    std::string log;
+    for(int pos1=0; pos1 < pages.size()-1; ++pos1)
     {
-        const auto p1 = pages[i];
-        const auto p2 = pages[i+1];
-
-        std::pair<PAGENUM, PAGENUM> p({p1, p2}); 
-
-        for(const auto rule : rule_book)
+        for(int pos2=pos1+1; pos2 < pages.size(); ++pos2)
         {
-            //std::cout << "Checking for rule: " << 
-            isOK = isOK && RULEDATA::is_according_to_rule(p, rule);
+            const auto p1 = pages[pos1];
+            const auto p2 = pages[pos2];
+
+            std::pair<PAGENUM, PAGENUM> p({p1, p2}); 
+
+            bool isDone = false;
+            int iRule = 0;
+
+            auto ruleIter = rule_book.begin();
+            for(; ruleIter != rule_book.end() && !isDone; ++ruleIter)
+            {
+                ++iRule;
+                auto rule = *ruleIter;
+
+                if(doPrint)
+                {
+                    log = "\t" + std::to_string(iRule) + " - Checking  \"" + p1 + " & " + p2 + "\" with  rule \"";
+                    log += rule._pg1st + "|" + rule._pg2nd;
+                    log += "\" : ";
+                }
+
+                isOK = isOK && RULEDATA::is_according_to_rule(p, rule);
+
+                std::string isOkRes = (isOK) ? "Good" : "Bad";
+                if(doPrint) log += isOkRes + "\n";
+
+                if(iRule == till)
+                {
+                    isDone=true;// to end this iteration;
+                    if(doPrint) log += "Done\n";
+                }
+                if(!isOK)
+                {
+                    std::cout << "LOG: " <<  log;
+                    return false;
+                }
+            }
+            //if(doPrint) std::cout << "\n";
         }
     }
     return isOK;
@@ -157,6 +214,7 @@ PGSVEC get_pages_vec(const std::string& page_str)
         page_vec.emplace_back(str);
         str.clear();
     }
+    return page_vec;
 }
 
 
@@ -166,7 +224,12 @@ void get_rules_and_pages_vec_from_file(
     std::vector<std::string>& candidate_pagno)
 {
     std::ifstream iFile(file_path);
-    
+    if(!iFile.is_open())
+    {
+        std::cout << "\n Failed to Open File: \"" << file_path << "\"\n";
+        return;
+    }
+
     std::string str;
     while(iFile >> str)
     {
@@ -184,23 +247,19 @@ void get_rules_and_pages_vec_from_file(
 
 
 void make_rules_book_from_strvec(
-    const std::vector<std::string>& crules,
-    std::vector<RULEDATA>& rule_book)
+    const std::vector<std::string>& c_rules,
+    RULESBOOK& rule_book)
 {
-    for(const auto s : crules)
+    for(const auto s : c_rules)
     {
-        RULEDATA rule1, rule2;
-
-        RULEDATA::make_rule(s , rule1, rule2);
-
-        rule_book.push_back(rule1);
-        rule_book.push_back(rule2);
+        //std::cout << "\nMaking rule from " << s ;
+        rule_book.push_back(RULEDATA::make_rule(s));
     }
 }
 
 void make_pages_list_from_strvec(
     const std::vector<std::string>& candidate_str,
-    std::list<PGSVEC>& pages_list)
+    PGSVECLST& pages_list)
 {
     for(const auto pg : candidate_str)
     {
@@ -210,11 +269,12 @@ void make_pages_list_from_strvec(
 
 void get_candidate_rules_and_pages(
     const std::string& fileP, 
-    std::vector<RULEDATA>& rule_book,
-    std::list<PGSVEC>& pages_list)
+    RULESBOOK& rule_book,
+    PGSVECLST& pages_list,
+    PGSVEC& ranks_list = PGSVEC())
 {
-    std::vector<std::string> candidate_rules;
-    std::vector<std::string> candidate_pagno;
+    PGSVEC candidate_pagno;
+    PGSVEC& candidate_rules = ranks_list;
 
     /*
     Read from file the given lists
@@ -225,10 +285,19 @@ void get_candidate_rules_and_pages(
     get_rules_and_pages_vec_from_file(fileP, candidate_rules, candidate_pagno);
 
     //Now generate a list of all the rules
-    //stored inside the RULEBOOK container
+    //stored inside the RULESBOOK container
     make_rules_book_from_strvec(candidate_rules, rule_book);
 
 
     //Extract all list of page numebrs 
     make_pages_list_from_strvec(candidate_pagno, pages_list);
+}
+
+
+
+void print_pages(const PGSVEC& pg)
+{
+    for(const auto& s : pg)
+        std::cout << s << " ";
+    std::cout<< "\n";
 }
