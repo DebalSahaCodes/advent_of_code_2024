0d62ddc949ef52fd77c8c2aee5651a809fd55392
Debal Saha <116924730+DebalSahaCodes@users.noreply.github.com>
2025-02-12 00:49:03 -0800

    Update day10.py
    updated but still need to fix out of range error

--- day10/day10.py
+++ day10/day10.py
@@ -6,6 +6,8 @@ class PosType:
     def __init__(self,x:int,y:int):
         self._x=x
         self._y=y
+    def pos_str(self) -> str:
+        return str(self._x) + "," + str(self._y)
 
 def solve_day10_part1(file_name):
     # read lines
@@ -39,22 +41,24 @@ def solve_day10_part1(file_name):
     n_alltrails=0
     for pos_th in pos_trail_head:
         not_done=1
-        trail_pos_list=[PosType] # storage of latest position
+        trail_pos_list=[] # storage of latest position
         # add the trail ehad as the first element , to act as the seed for the rest of the process
         trail_pos_list.append(pos_th)
         # while-loop till all trails are explored
+        print("\n\nStarting trail starting at (" + pos_th.pos_str() + ") ...")
         while(not_done):
-            candidate_pos=[PosType] # list to store all the possible positions leading to full trail
-            try_direction=[PosType] # temp list to stroe positions in all directions to try
+            candidate_pos=[] # list to store all the possible positions leading to full trail
             for old_pos in trail_pos_list:
                 old_x=old_pos._x
                 old_y=old_pos._y
                 c_old_num=lines[old_x][old_y]
+                print("\nSearching for trail " + c_old_num + " at (" + old_pos.pos_str() + ") ...")
+                try_direction=[] # temp list to stroe positions in all directions to try
                 # gather all possible positions in all 4 directions of this position
-                try_direction.append(PosType(x,y-1))
-                try_direction.append(PosType(x+1,y))
-                try_direction.append(PosType(x,y+1))
-                try_direction.append(PosType(x-1,y))
+                try_direction.append(PosType(old_x,old_y-1))
+                try_direction.append(PosType(old_x+1,old_y))
+                try_direction.append(PosType(old_x,old_y+1))
+                try_direction.append(PosType(old_x-1,old_y))
                 # iterate over this 4 possible positions directioned at N,E,S,W and see if expected num found
                 for tdir in try_direction:
                     x=tdir._x
@@ -66,12 +70,16 @@ def solve_day10_part1(file_name):
                         # compare expected num with the num in the list
                         old_num=int(c_old_num)
                         if new_num==old_num+1:
-                            print("Found ", c_new_num)
-                            candidate_pos.append(pos)
+                            print("Found ", c_new_num + " at (" + str(x) + "," + str(y) + ") !")
+                            candidate_pos.append(tdir)
                             if c_new_num=='9':
-                                not_done=1
+                                not_done=0
                                 n_alltrails += 1
                                 print("Done for trail head and till now total trails=", n_alltrails)
+                        else: #bad path
+                            print("No path at (" + str(x) + "," + str(y) + ") !")
+                    else:
+                        print("Bad position coordinates: " + str(x) + "," + str(y))
                 # once done checking all 4 directioned positions numbers, then assign this list to pos_list to redo same process
                 trail_pos_list = candidate_pos
 
