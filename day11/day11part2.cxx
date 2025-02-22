6b271509dda728f25b35e258d24e58188320b1ce
Debal Saha <116924730+DebalSahaCodes@users.noreply.github.com>
2025-02-21 22:53:31 -0800

    use the helper functions for file based string processing

--- day11/day11part2.cxx
+++ day11/day11part2.cxx
@@ -1,94 +1,5 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<list>
-#include<chrono>
 
-
-using STRLST=std::list<std::string>;
-using STRITR=std::list<std::string>::iterator;
-
-bool apply_rule_1(STRITR& itr)
-{
-    //std::cout << "\n ... applying rule 1 ...";
-    if (*itr == "0")
-    {
-        *itr = "1";
-        //std::cout << " added " << *itr << ".. ; applied...\n\n";
-        return true;
-    }
-    else
-    {
-        //std::cout << "; nope...";
-        return false;
-    }
-}
-
-// splits the num-string in two
-bool apply_rule_2(STRITR& itr, STRLST& lst)
-{
-    //std::cout << "\n ... applying rule 2 to \"" << *itr << "\" ... ";
-
-    if(itr->size()%2==0)
-    {
-        // find middle position 
-        auto pos_mid = itr->size()/2; //std::cout<< "; mid pos : " << pos_mid;
-        // obtain the later half of string
-        auto str2 =itr->substr(pos_mid);
-        str2 = std::to_string(std::stoll(str2));
-        // make the current string first-half
-        *itr=itr->substr(0, pos_mid);
-        // find the later-half string insertion position
-        auto itr_insert = std::next(itr);
-        // insert the later-half string in the list
-        lst.insert(itr_insert, str2);
-
-        //std::cout << " ... created " << *itr << " and " << str2 << ".. ; applied...\n\n";
-        return true;
-    }
-    else
-    {
-        //std::cout << "; nope...";
-        return false;
-    }
-}
-
-void apply_rule_3(STRITR& itr)
-{
-    //std::cout << "\n ... applying rule 3 ...";
-    *itr = std::to_string(std::stoll(*itr)*2024);
-    //std::cout << " added " << *itr << "..\n\n";
-}
-
-
-bool get_num_str_list(const std::string&  file_path, STRLST& str_lst)
-{
-    std::ifstream f_handle(file_path);
-
-    if(!f_handle.is_open())
-        return false;
-
-    std::string num_str;
-    while(f_handle >> num_str)
-    {
-        str_lst.emplace_back(num_str);
-    }
-    f_handle.close();
-
-    return true;
-}
-
-void print_stones(const STRLST& strl, const int blink_count)
-{
-    std::cout << "\nAfter " << blink_count << " blinks, no. of stones : " << strl.size()
-              << ", the string is  : \n";
-
-    for(const auto s : strl)
-    {
-        std::cout << s << " " ;
-    }
-    std::cout << "\n";
-}
+#include "helper_day11part2.hpp"
 
 int main(int argc, char** argv)
 {
@@ -96,9 +7,6 @@ int main(int argc, char** argv)
 
     std::string file_p="puzzle_input.txt";
     //auto file_p="sample_2.txt";
-
-
-    auto start_time = std::chrono::high_resolution_clock::now();
 
 
     // if user input given then use those
@@ -148,38 +56,47 @@ int main(int argc, char** argv)
     }
     std::cout << "\n";
 
-    int idx_counter = 0;
+    auto start_time = std::chrono::high_resolution_clock::now();
 
-    while(idx_counter++ < blink_count)
+    bool stonesdone=false;
+    int curr_idx=1;
+    try
     {
-        for(auto itr = numstr_list.begin(); itr != numstr_list.end(); ++itr)
-        {
-            if(apply_rule_1(itr))
-            {
-                continue;
-            }
-            else if(apply_rule_2(itr, numstr_list))
-            {
-                ++itr;
-                continue;
-            }
-            else
-            {
-                apply_rule_3(itr);
-                //std::cout << "\n";
-            }
-        }
-        print_stones(numstr_list, idx_counter);
+        perform_stone_blinks(blink_count, numstr_list, curr_idx);
+    }
+    catch(...)
+    {
+        std::cout << "\n Failed at idx \"" << curr_idx << " \" !!!\n\n";
     }
 
+    if(!stonesdone)
+    {
+        constexpr bool dump_it = true;
+        print_stones(numstr_list, curr_idx, dump_it);
+    }
 
-    //print_stones(numstr_list, blink_count);
-
+    /*try
+    {
+        // print the solution
+        print_stones(numstr_list, blink_count);
+    }
+    catch(...)
+    {
+        std::cout << "\n Failed to print the stones of size \""
+                  << numstr_list.size() << " \" after "
+                  << blink_count << " blinks!!!\n\n";
+    }*/
+    std::cout << "\n Size of the stones \"" << numstr_list.size() << "\"\n";
 
     auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
 
-    std::cout << "\nTime taken: " << duration << " ms\n";
+    auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+    auto duration__s = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time).count();
+
+    bool is_sec = duration__s>0;
+    auto duration = (is_sec ? duration__s : duration_ms);
+
+    std::cout << "\nTime taken: " << duration << (is_sec ? " s" : " ms") << "\n";
 
     return 0;
 }
