460f1572d9e3e8fca386791451da9ee786631e98
Debal Saha <116924730+DebalSahaCodes@users.noreply.github.com>
2025-03-13 23:33:30 -0700

    corrected solution in part1.py

--- day12/part1.py
+++ day12/part1.py
@@ -46,41 +46,56 @@ def area_list_connected_to_e(area_list, e:AreaElement):
             return 1
     return is_connected
 
+
 def add_to_area_dict(area_dict, area_c, area_e:AreaElement):
     dict={}
-    need_reconnect=0
-    x_idx=-1
-    x_str=''
+    is_inserted=0
+    is_merging=0
+    x_a_char='' # to be removed post merge
     n_char=0 # default index added to character when mapping to area-dict
     for ad in area_dict:
+        # the key in dict is of format CHAR_NUM e.g. "C_10"
+        # split the key with "_" as delimiter;so 
+        # - CHAR="C" 
+        # - NUM="11"
         ad_str=ad.split('_')
-        # if the given character is found in the area dict
-        if len(ad_str)>1 and ad_str[0]==area_c:
-            # if connection found and the index > 0 then need to reconnect
+        if len(ad_str)<=1:
+            print("BAD DICT ELEMENT CHAR", ad,"\nEXITING...")
+            exir()
+        a_chr=ad_str[0]
+        a_num=int(ad_str[1])
+        # if the CHAR part matches the current character in AreaElement
+        if a_chr==area_c:
+            # if area to which current element is connected, is found, then insert
             if area_list_connected_to_e(area_dict[ad], area_e):
-                need_reconnect=int(ad_str[1])>0 and area_c in dict and int(ad_str[1])!=dict[area_c]
-                if need_reconnect:
-                    x_idx = dict[area_c]
-                    x_str = ad
-                    break
-            # if that character is not connected to the found area
-            elif not area_list_connected_to_e(area_dict[ad], area_e):
-                n_char+=1 # increment the index for mapping to area-dict
-        # store the index for next iteration comparison
-        dict[area_c] = int(ad_str[1])
-    if not need_reconnect:
-        print("\tAdding ",area_c + '_' + str(n_char))
+                if not is_inserted:
+                    print("\tAdding to ", ad)
+                    area_dict[ad].append(area_e)
+                    is_inserted = 1
+                    # store the index in a local dict for next iteration
+                    dict[area_c] = ad#int(ad_str[1])
+                    continue
+                else:
+                    is_merging=1 # already inserted and found connected to current area
+                    print("\tMerging ", ad, " into ", dict[area_c])
+                    for t in area_dict[ad]:
+                        area_dict[dict[area_c]].append(t)
+                    x_a_char = ad # set the merged list for removal
+            else: #if not connected
+                n_char+=1
+
+    if not is_inserted:
+        print("\tInsert ",area_c + '_' + str(n_char))
         area_dict[area_c + '_' + str(n_char)].append(area_e)
-    else:
-        print("\tMerging ", x_str, " into ", area_c + '_' + str(x_idx))
-        tmp = area_dict[x_str]
-        for t in tmp:
-            area_dict[area_c + '_' + str(x_idx)].append(t)
-        area_dict[area_c + '_' + str(x_idx)].append(area_e)
-        # done moving now pop the last created list
-        area_dict.pop(x_str)
+
+    if x_a_char and is_merging: # if non empty list remaining that was merged then pop from area_dict
+        print("\tRemoving merged list ", x_a_char)
+        area_dict.pop(x_a_char)
+
+
 
 def get_elemnent(element, pos_x, pos_y, lines):
+    str1=""
     min_x=0
     min_y=0
     max_x=len(lines[0])-1 # as there is "\n" as extra character
@@ -97,6 +112,15 @@ def get_elemnent(element, pos_x, pos_y, lines):
     has_dn=dn_x>=0 and dn_x<max_x and dn_y>=0 and dn_y<max_y and lines[dn_y][dn_x]==element
     has_rt=rt_x>=0 and rt_x<max_x and rt_y>=0 and rt_y<max_y and lines[rt_y][rt_x]==element
     has_lt=lt_x>=0 and lt_x<max_x and lt_y>=0 and lt_y<max_y and lines[lt_y][lt_x]==element
+    #if has_up:
+    #    str1 +=" has_up"
+    #if has_dn:
+    #    str1 +=" has_dn"
+    #if has_rt:
+    #    str1 +=" has_rt"
+    #if has_lt:
+    #    str1 +=" has_lt"
+    #print(element, str1)
     return AreaElement(element,has_lt,has_rt,has_up,has_dn,pos_x,pos_y)
 
 
@@ -115,8 +139,9 @@ def get_perimeter_value_for_element(ar_el:AreaElement):
 
 if __name__=='__main__':
 
-    #fileP ="puzzle_input.txt"
-    fileP ="sample_input.txt"
+    fileP ="puzzle_input.txt"
+    #fileP ="sample_input.txt"
+    #fileP ="sample_input1.txt"
     fH = open(fileP, "r")
     lines = fH.readlines()
     fH.close()
@@ -128,11 +153,13 @@ if __name__=='__main__':
         pos_x=0
         for c in line:
             if c !='\n':
-                e = get_elemnent(areas, pos_x, pos_y, lines)
+                e = get_elemnent(c, pos_x, pos_y, lines)
                 add_to_area_dict(areas, c, e)
             pos_x +=1
         pos_y+=1
 
+
+    total_cost=0
     for area_l in areas:
         area_c = area_l
         area_p = areas[area_l]
@@ -141,4 +168,6 @@ if __name__=='__main__':
         peri=0
         for e in area_p:
             peri += get_perimeter_value_for_element(e)
-        print("For \"", area_c,"\" area:",area_v," peri:", peri," cost=",area_v*peri)
+        print("Region ", area_c," with price ",area_v,"*", peri," = ",area_v*peri)
+        total_cost += area_v*peri
+    print("\n\nTotal_cost: ", total_cost)
