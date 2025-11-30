c998da0993034372852f42efd31f11670515656c
Debal Saha <116924730+DebalSahaCodes@users.noreply.github.com>
2025-11-30 12:19:54 -0800

    Updated maze map to store min score for each maze position and skip iteration if p1 score for pos it not below min, solves d16p1 puzzle

--- day16/cp1_simple.cpp
+++ day16/cp1_simple.cpp
@@ -10,7 +10,7 @@
 
 std::string g_iFileName="puzzle.txt";
 
-std::map<std::pair<int,int>,char> g_maze;
+std::map<std::pair<int,int>,std::pair<char,int>> g_maze;
 
 std::tuple<int, int, char> S_pos;
 std::tuple<int, int, char> E_pos;
@@ -23,9 +23,9 @@ std::list<int> g_scores;
 //std::map<std::pait<int,int>,char> g_DICT;
 
 std::string get_str(const std::tuple<int, int, char>& tup){
-    return std::to_string(std::get<0>(tup)) + ","
+    return "(" + std::to_string(std::get<0>(tup)) + ","
            + std::to_string(std::get<1>(tup)) + ","
-           + std::get<2>(tup);
++ std::get<2>(tup) + ")";
 }
 
 void initialize()
@@ -48,7 +48,7 @@ void initialize()
             continue;
         }
         auto pos = std::make_pair(idx, idy);
-        g_maze.insert({pos, ch});
+        g_maze.insert({pos, std::make_pair(ch,0)});
         //std::cout << "\n" << itr++ << ": " << pos.m_x << "," << pos.m_y << " : " << ch;
         if(ch=='S'){
             S_pos=std::make_tuple(idx,idy,'S');
@@ -65,8 +65,8 @@ void initialize()
     g_xMax=idx-1;
     g_yMax=idy;
 
-    std::cout<<"xMax:" << g_xMax << "\n";
-    std::cout<<"yMax:" << g_yMax << "\n";
+    std::cout<<"g_xMax:" << g_xMax << "\n";
+    std::cout<<"g_yMax:" << g_yMax << "\n";
     std::cout<<"S-Pos:" << get_str(S_pos) << "\n";
     std::cout<<"E-Pos:" << get_str(E_pos) << "\n";
 
@@ -83,15 +83,28 @@ LTYPE get_next(const RTYPE& given_v)
     const auto& pos01 = std::get<0>(given_v);
     const std::tuple<int, int, char>& p0 = pos01.first;
     const std::tuple<int, int, char>& p1 = pos01.second;
+    //std::cout << "\n\t: processing p1 " << get_str(p1) << "\n";
 
     const int score = std::get<1>(given_v);
     const int min_score = (g_scores.empty()) ? -9999 : g_scores.front();
     //std::cout << "\n\t score: " << score << " and min. " << min_score;
 
-    const char c_p1 = g_maze[std::make_pair(std::get<0>(p1),std::get<1>(p1))];
+    const char c_p1 = g_maze[std::make_pair(std::get<0>(p1),std::get<1>(p1))].first;
     const bool found_E = c_p1=='E';
     const bool found_H = c_p1=='#';
-    const bool found_MIN = !g_scores.empty() && score >= g_scores.front();
+    bool found_MIN = !g_scores.empty() && score >= g_scores.front();
+    // do another current min. score for the position in MAZE based check
+    // if for the given position in maze there exists a smaller score that means
+    // the current path is not worth traversing
+    int& score_p1 = g_maze[std::make_pair(std::get<0>(p1),std::get<1>(p1))].second;
+    if(score_p1 ==0 || score < score_p1){
+        // this branch updates the MAZE position score with the current min. score
+        score_p1=score;
+    } else if(score_p1 !=0 && score >= score_p1){
+        // this branch sets FLAG when current min. is smaller than the this iteration score
+        found_MIN=true;
+    }
+
     const bool is_end_condition = found_E || found_H || found_MIN;
     if(found_E) {
         std::cout << "\n FOUND \"E\".. adding score-" << g_scores.size() << " -> " << score << "\n";
@@ -99,7 +112,7 @@ LTYPE get_next(const RTYPE& given_v)
         g_scores.sort();
     }
     else if(found_MIN){
-        //std::cout << " not better than current min. " << g_scores.front() << "\n";
+        std::cout << " not better than current min. " << g_scores.front() << "\n";
     }
 
     if(false == is_end_condition){
@@ -138,11 +151,14 @@ LTYPE get_next(const RTYPE& given_v)
                 std::cout << "\n Reversal FOund!!" << get_str(p0) << " vs. " << get_str(p_next);
                 continue;
             }else if(std::get<0>(p_next)<0 || std::get<0>(p_next)>g_xMax){
+                std::cout << "\n\t: X out of bound\n";
                 continue;
             }else if(std::get<1>(p_next)<0 || std::get<1>(p_next)>g_yMax){
+                std::cout << "\n\t: Y out of bpund!!\n";
                 continue;
             }else{
                 const auto& p_old = p1;
+                //std::cout << "\n\t: adding " << get_str(p_old)  << " -> " << get_str(p_next) << " score: " << p_score <<"\n";
                 v_list.push_back({std::make_pair(p_old, p_next), p_score});
             }
         }
@@ -163,6 +179,8 @@ int main(){
             const LTYPE& x_List = get_next(u);
             if(!x_List.empty()){
                 std::copy(x_List.begin(), x_List.end(), std::back_inserter(c_List));
+            } else{
+                //std::cout << "\n RETURNED EMPTY!!";
             }
         }
         if(c_List.empty())
